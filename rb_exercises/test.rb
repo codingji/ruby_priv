3702d0c5b0e65602d47648fbd0d4dd4084cb1d6c
codingji <codingji@protonmail.com>
2023-05-08 16:51:32 -0700

    Finished 1.1-11 Starting Exercises Easy 5 - 7 before starting quiz.

--- rb_exercises/test.rb
+++ rb_exercises/test.rb
@@ -1,19 +1,2 @@
 require 'pry'
 
-numbers = [1, 2, 3, 4]
-
-# p(
-# numbers.each do |number|
-#   p number
-#   numbers.shift(1)
-# end
-# )
-
-
-p(
-  numbers.each do |number|
-    binding.pry
-    p number
-    numbers.pop(1)
-  end
-)
